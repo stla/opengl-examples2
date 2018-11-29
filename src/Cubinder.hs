1f62da896d4798f0f23ef475ea89ac76a2a88691
stla <laurent_step@yahoo.fr>
2018-11-29 14:52:23 +0100

    cubinder

--- src/Cubinder.hs
+++ src/Cubinder.hs
@@ -1,77 +1,62 @@
-module TwentyCones
+module Cubinder
   where
 import           Control.Concurrent                (threadDelay)
 import           Control.Monad                     (when, forM_)
+import           Cubinder.Data
 import qualified Data.ByteString                   as B
 import           Data.IORef
-import           Data.Vector                       (Vector, (!))
+import           Data.Tuple.Extra                  (both)
+import           Data.Vector                       ((!))
 import           Graphics.Rendering.OpenGL.Capture (capturePPM)
 import           Graphics.Rendering.OpenGL.GL
 import           Graphics.UI.GLUT
-import           Linear                             (V3 (..), (*^))
+import           Linear                            (V3 (..))
 import           System.Directory                  (doesDirectoryExist)
 import           System.IO.Unsafe                  (unsafePerformIO)
 import           Text.Printf
 import           Utils.ConeMesh
-
-type Mesh = (Vector ((Float,Float,Float),(Float,Float,Float)), 
-             [(Int,Int,Int,Int)])
 
 data Context = Context
     {
       contextRot1      :: IORef GLfloat
     , contextRot2      :: IORef GLfloat
     , contextRot3      :: IORef GLfloat
-    , contextAlpha     :: IORef GLfloat
+    , contextAlpha     :: IORef Double
     , contextZoom      :: IORef GLdouble
     }
 
-white,blue,green :: Color4 GLfloat
+white,black,whitesmoke :: Color4 GLfloat
 white      = Color4    1    1    1    1
-blue       = Color4    0    0    1    1
-green      = Color4    0    1    0    1
+black      = Color4    0    0    0    1
+whitesmoke = Color4 0.96 0.96 0.96    1
 
-toVx3 :: (Float,Float,Float) -> Vertex3 Float
+toVector3 :: Floating a => (a,a,a) -> Vector3 a
+toVector3 (x,y,z) = Vector3 x y z
+
+toVx3 :: Floating a => (a,a,a) -> Vertex3 a
 toVx3 (x,y,z) = Vertex3 x y z
 
-toN3 :: (Float,Float,Float) -> Normal3 Float
+toV3 :: Floating a => (a,a,a) -> V3 a
+toV3 (x,y,z) = V3 x y z
+
+toN3 :: Floating a => (a,a,a) -> Normal3 a
 toN3 (x,y,z) = Normal3 x y z
 
-phi,a,b,c :: GLfloat
-phi = (1 + sqrt 5) / 2
-a = 1 / sqrt 3
-b = a / phi
-c = a * phi
+stereog :: Double -> (Double,Double,Double,Double) -> (Double,Double,Double)
+stereog r (x1,x2,x3,x4) = (x1 / (r-x4), x2 / (r-x4), x3 / (r-x4))
 
-points :: [V3 GLfloat]
-points = 
-   [V3 a a a,
-    V3 a a (-a),
-    V3 a (-a) a,
-    V3 (-a) (-a) a,
-    V3 (-a) a (-a),
-    V3 (-a) a a,
-    V3 0 b (-c),
-    V3 0 (-b) (-c),
-    V3 0 (-b) c,
-    V3 c 0 (-b),
-    V3 (-c) 0 (-b),
-    V3 (-c) 0 b,
-    V3 b c 0,
-    V3 b (-c) 0,
-    V3 (-b) (-c) 0,
-    V3 (-b) c 0,
-    V3 0 b c,
-    V3 a (-a) (-a),
-    V3 c 0 b,
-    V3 (-a) (-a) (-a)]
-
-points' :: [V3 GLfloat]
-points' = map ((*^) 0.1) points
-
-meshesAndMatrices :: [(Mesh, [Float])]
-meshesAndMatrices = 
-    map (\(pt,pt') -> coneMesh pt pt' 0.1 0.03 3 60) (zip points points')
+rightIsoclinic :: Double -> Double -> Double 
+               -> (Double,Double,Double,Double) -> (Double,Double,Double,Double) 
+rightIsoclinic theta phi alpha (x0,x1,x2,x3) =
+  ( q0*x0 - q1*x1 - q2*x2 - q3*x3
+  , q1*x0 + q0*x1 + q3*x2 - q2*x3
+  , q2*x0 - q3*x1 + q0*x2 + q1*x3
+  , q3*x0 + q2*x1 - q1*x2 + q0*x3 )
+  where
+    q0 = cos alpha
+    q1 = sin theta * cos phi * sin alpha
+    q2 = sin theta * sin phi * sin alpha
+    q3 = cos theta * sin alpha
 
 display :: Context -> DisplayCallback
 display context = do
@@ -81,54 +66,63 @@ display context = do
   r3 <- get (contextRot3 context)
   zoom <- get (contextZoom context)
   alpha <- get (contextAlpha context)
+  let points  = map (rightIsoclinic 0 0 (alpha * pi / 180)) vertices
+      ppoints = map (stereog (sqrt 2)) points
+      vectors = map toVector3 ppoints
+      edges'  = map (both (toV3 . (!!) ppoints)) edges
+      -- meshesAndMatrices :: [(Mesh, [Float])]
+      meshesAndMatrices = 
+          map (\(pt,pt') -> coneMesh pt pt' 0.08 0.08 3 45) edges'
   loadIdentity
   (_, size) <- get viewport
   resize zoom size
   rotate r1 $ Vector3 1 0 0
   rotate r2 $ Vector3 0 1 0
   rotate r3 $ Vector3 0 0 1
-  rotate alpha $ Vector3 1 1 1
+  forM_ vectors $ \vec -> preservingMatrix $ do
+    translate vec
+    materialDiffuse Front $= whitesmoke
+    renderObject Solid $ Sphere' 0.1 15 15
   forM_ meshesAndMatrices $ \meshAndMatrix -> 
     preservingMatrix $ do
-      m <- newMatrix RowMajor (snd meshAndMatrix) :: IO (GLmatrix GLfloat)
+      m <- newMatrix RowMajor (snd meshAndMatrix) :: IO (GLmatrix Double)
       multMatrix m
       forM_ ((snd . fst) meshAndMatrix) $ \(i,j,k,l) ->
         renderPrimitive Quads $ do
-          materialDiffuse Front $= blue
-          materialDiffuse Back $= green
+          materialDiffuse Front $= whitesmoke
           drawQuad i j k l ((fst . fst) meshAndMatrix)
   swapBuffers
   where
     drawQuad i j k l verticesAndNormals = do 
-      normal ni
-      vertex vi
-      normal nj
-      vertex vj
-      normal nk
-      vertex vk
-      normal nl
-      vertex vl
-      where
-        (vi',ni') = verticesAndNormals ! i
-        (vj',nj') = verticesAndNormals ! j
-        (vk',nk') = verticesAndNormals ! k
-        (vl',nl') = verticesAndNormals ! l
-        vi = toVx3 vi'
-        ni = toN3 ni'
-        vj = toVx3 vj'
-        nj = toN3 nj'
-        vk = toVx3 vk'
-        nk = toN3 nk'
-        vl = toVx3 vl'
-        nl = toN3 nl'
-
+        normal ni
+        vertex vi
+        normal nj
+        vertex vj
+        normal nk
+        vertex vk
+        normal nl
+        vertex vl
+        where
+          (vi',ni') = verticesAndNormals ! i
+          (vj',nj') = verticesAndNormals ! j
+          (vk',nk') = verticesAndNormals ! k
+          (vl',nl') = verticesAndNormals ! l
+          vi = toVx3 vi'
+          ni = toN3 ni'
+          vj = toVx3 vj'
+          nj = toN3 nj'
+          vk = toVx3 vk'
+          nk = toN3 nk'
+          vl = toVx3 vl'
+          nl = toN3 nl'
+      
 resize :: GLdouble -> Size -> IO ()
 resize zoom s@(Size w h) = do
   viewport $= (Position 0 0, s)
   matrixMode $= Projection
   loadIdentity
   perspective 45.0 (realToFrac w / realToFrac h) 1.0 100.0
-  lookAt (Vertex3 0 0 (2.8+zoom)) (Vertex3 0 0 0) (Vector3 0 1 0)
+  lookAt (Vertex3 0 0 (6.3+zoom)) (Vertex3 0 0 0) (Vector3 0 1 0)
   matrixMode $= Modelview 0
 
 keyboard :: IORef GLfloat -> IORef GLfloat -> IORef GLfloat -- rotations
@@ -159,7 +153,7 @@ ppmExists :: Bool
 {-# NOINLINE ppmExists #-}
 ppmExists = unsafePerformIO $ doesDirectoryExist "./ppm"
 
-idle :: IORef Bool -> IORef Bool -> IORef Int -> IORef Int -> IORef GLfloat 
+idle :: IORef Bool -> IORef Bool -> IORef Int -> IORef Int -> IORef Double 
      -> IdleCallback
 idle anim save delay snapshots alpha = do
   an <- get anim
@@ -183,11 +177,10 @@ main = do
   _ <- createWindow "Twenty cones"
   windowSize $= Size 500 500
   initialDisplayMode $= [RGBAMode, DoubleBuffered, WithDepthBuffer]
-  clearColor $= white
-  materialSpecular FrontAndBack $= white
-  materialShininess FrontAndBack $= 50
+  clearColor $= black
+  materialSpecular Front $= white
+  materialShininess Front $= 50
   lighting $= Enabled
-  lightModelTwoSide $= Enabled
   light (Light 0) $= Enabled
   position (Light 0) $= Vertex4 0 0 100 1
   specular (Light 0) $= white
@@ -211,7 +204,7 @@ main = do
     Just (keyboard rot1 rot2 rot3 zoom anim save delay)
   snapshot <- newIORef 0
   idleCallback $= Just (idle anim save delay snapshot alpha)
-  putStrLn "*** Twenty cones ***\n\
+  putStrLn "*** Cubinder ***\n\
         \    To quit, press q.\n\
         \    Scene rotation: e, r, t, y, u, i\n\
         \    Zoom: l, m\n\
--- src/Cubinder/Data.hs
+++ src/Cubinder/Data.hs
@@ -2,8 +2,8 @@ module Cubinder.Data where
 import Data.Maybe (isJust, fromJust)
 
 n1,n2 :: Int
-n1 = 4
-n2 = 30
+n1 = 30
+n2 = 4
 
 vertices :: [(Double,Double,Double,Double)]
 vertices = map flatten [(u,v) | u <- poly1, v <- poly2]
@@ -15,20 +15,22 @@ vertices = map flatten [(u,v) | u <- poly1, v <- poly2]
   poly2 = [(cos(i*2*pi / toDbl n2),sin(i*2*pi / toDbl n2)) | 
            i <- map toDbl [0 .. n2-1]]
   flatten :: ((Double,Double),(Double,Double)) -> (Double,Double,Double,Double)
-  flatten ((u1,u2),(v1,v2)) = (u1,v1,u2,v2)
+  flatten ((u1,u2),(v1,v2)) = (u1,u2,v1,v2)
 
 edges_ij :: (Int,Int) -> [(Int,Int)]
 edges_ij (i,j) = map fromJust $ filter isJust [e1, e2, e3, e4]
   where
+    toInt :: (Int,Int) -> Int
+    toInt (k1,k2) = k1*n2 + k2
     e = (i, j)
     c1 = (i, (j-1) `mod` n2)
-    e1 = if e<c1 then Just c1 else Nothing
+    e1 = if e<c1 then Just (toInt e, toInt c1) else Nothing
     c2 = (i, (j+1) `mod` n2)
-    e2 = if e<c2 then Just c2 else Nothing
+    e2 = if e<c2 then Just (toInt e, toInt c2) else Nothing
     c3 = ((i-1) `mod` n1, j)
-    e3 = if e<c3 then Just c3 else Nothing
+    e3 = if e<c3 then Just (toInt e, toInt c3) else Nothing
     c4 = ((i+1) `mod` n1, j)
-    e4 = if e<c4 then Just c4 else Nothing
+    e4 = if e<c4 then Just (toInt e, toInt c4) else Nothing
 
 edges :: [(Int, Int)]
 edges = concatMap edges_ij [(i,j) | i <- [0 .. n1-1], j <- [0 .. n2-1]]
